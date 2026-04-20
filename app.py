6730443982345df2cb373130dad8986cf2acac33
Bruno Amorim <brunojfgbilusbgbvdsg@gmail.com>
2026-04-20 17:09:51 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -31,5 +31,6 @@ def download():
         print(f"Erro no processamento: {e}")
         return f"Erro ao processar o vídeo: {str(e)}", 500
     
-if __name__ == '__main__':    
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
